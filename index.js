f365f6706f3c6dce617047c8fa825ea25c95cdfb
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-05-20 09:24:25 -0400

    whitelisting feature is here

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const config = require ('./lib/config')();
+const express = require ('express');
+const app = express();
+
+app.get('/geth/:scope?/:task?', function(req, res, next){
+  const ipv4 = req.ip.split(':').pop();
+  if (config.whitelist.indexOf(ipv4) >= 0) next();
+  res.status(403).end();
+});
+
+app.get('/geth/:scope?/:task?', function (req, res) {
+  console.log(req.params);
+  res.send('Hello Kong!');
+
+});
+
 console.log(config);
+
+app.listen(config.PORT || 1838);
